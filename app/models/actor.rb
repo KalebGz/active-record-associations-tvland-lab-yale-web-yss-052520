4e30254169d8f7f22c872fc565c30b8400a24e72
KalebGz <kaleb.zeray@gmail.com>
2020-06-07 21:12:35 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    first_name + " " + last_name
+  end
+
+  def list_roles
+    roles = []
+    roles.push(Character.all.where(:actor == self).map{|char| char.name + " - " + char.show.name})
+    roles[0]
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list
+        Character.all.where(:show == self).map{|char| char.actor.full_name}
+    end
 end
